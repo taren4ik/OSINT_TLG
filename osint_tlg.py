5782cc18f5e78c2cb555fef58d8edb166da78c9d
Dmitry Permyakov <taren4ik@mail.ru>
2022-05-21 23:28:26 +1000

    Сделал запуск клиента в боте.

--- osint_tlg.py
+++ osint_tlg.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from telegram import ReplyKeyboardMarkup, Bot
 from multiprocessing import Process
 from telegram.ext import CommandHandler, MessageHandler, Updater, Filters
+from asyncio import set_event_loop, new_event_loop
 
 load_dotenv()
 
@@ -33,29 +34,46 @@ def wake_up(update, context):
 def parsing(update, context):
     """ Парсинг пользователей чата."""
     chat = update.effective_chat
-    # buttons = ReplyKeyboardMarkup([['/people'], ['/message']],
-    #                               resize_keyboard=True)
+    buttons = ReplyKeyboardMarkup([['/people'], ['/message']],
+                                  resize_keyboard=True)
     context.user_data['chat_name'] = update.message.text
     # Используя контекст context.user_data
-    # context.bot.send_message(
-    #     chat_id=chat.id,
-    #     text='Выберете тип отчета ',
-    #     reply_markup=buttons,
-    # )
-    print(context.user_data.get('chat_name', 'Not found'))
-    procs = []
-    proc = Process(target=msg_parse, args=(context.user_data['chat_name']),)
-    procs.append(proc)
-    proc.start()
+    context.bot.send_message(
+        chat_id=chat.id,
+        text='Выберете тип отчета ',
+        reply_markup=buttons,
+    )
+    # print(context.user_data.get('chat_name', 'Not found'))
+    # procs = []
+    # proc = Process(target=msg_parse, args=(context.user_data['chat_name']), )
+    # procs.append(proc)
+    # proc.start()
+    # for proc in procs:
+    #     proc.join()
 
 
-def msg_parse(chat):
-    url = f'https://t.me/{chat}'
+def msg_parse(update, context):
+    # Получаем информацию о чате, из которого пришло сообщение,
+    # и сохраняем в переменную chat
+    user_chat = update.effective_chat
+    #context.user_data['chat_name'] = update.message.text
+    chat = context.user_data['chat_name']
+    # proc = Process(target=client, args=(chat, user_chat), )
+    # proc.start()
+    # print('Start')
+    # proc.join()
+
+    set_event_loop(new_event_loop())
+    client(chat, user_chat)
+
+
+def client(chat, url):
     user_ids = []
     first_names = []
     last_names = []
     usernames = []
     standart_phones = []
+    url = f'https://t.me/{chat}'
     app = TelegramClient('osint', api_id, api_hash)
     app.start()
     participants = app.get_participants(url)  # получаем список участников
@@ -79,15 +97,16 @@ def people_parse(update, context):
 
 
 def main():
-    while True:
-        updater = Updater(token)
-        updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-        # updater.dispatcher.add_handler(
-        # CommandHandler('people', people_parse))
-        # updater.dispatcher.add_handler(CommandHandler('message', msg_parse))
-        updater.dispatcher.add_handler(MessageHandler(Filters.text, parsing))
-        updater.start_polling()
-        updater.idle()
+    updater = Updater(token)
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('message', msg_parse))
+
+    updater.dispatcher.add_handler(
+        CommandHandler('people', people_parse))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, parsing))
+
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
