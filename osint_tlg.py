6bc395920a86463cef61d78937c9f9ee3b1325bd
Dmitry Permyakov <taren4ik@mail.ru>
2022-07-10 20:14:46 +1000

    Добавил выгрузку сообщений из чата.

--- osint_tlg.py
+++ osint_tlg.py
@@ -1,3 +1,5 @@
+#TODO сделать логгирование запросов и ограничение на крличество бесплатных
+#TODO сделать последовательную выгрузку для больших чатов
 import os
 
 import pandas as pd
@@ -46,12 +48,46 @@ def parsing(update, context):
     )
 
 
+def get_message(chat):
+    msg_id = []
+    msg_dt = []
+    msg_txt = []
+    url = f'https://t.me/{chat}'
+
+    app = TelegramClient('osint', api_id, api_hash)
+    app.start()
+    messages = app.get_messages(url, 5000)
+    for msg in messages:
+        msg_txt.append(str(msg.message))
+        msg_id.append(str(msg.sender_id))
+        msg_dt.append(str(msg.date))
+    df_list = pd.DataFrame(
+        {'Id': msg_id, 'DateTime': msg_dt, 'Message': msg_txt})
+    app.disconnect()
+    return df_list
+
+
 def msg_parse(update, context):
     """Прасинг сообщений."""
-    pass
+    user_chat = update.effective_chat
+    context_user = context.user_data['chat_name']
+    if context_user.find(r'https://t.me/') != -1:
+        chat = context.user_data['chat_name'].split(r'/')[3]
+    else:
+        chat = context.user_data['chat_name']
+
+    set_event_loop(new_event_loop())
+    df_list = get_message(chat)  # получаем данные
+    df_list.to_csv(f'{chat}.csv', sep=';', header=True, index=False)
+    path = os.path.abspath(f'{chat}.csv')
+    context.bot.send_document(
+        chat_id=user_chat.id,
+        document=open(f'{path}', 'rb')
+    )
+    os.remove(path)
 
 
-def client(chat):
+def get_people(chat):
     user_ids = []
     first_names = []
     last_names = []
@@ -74,10 +110,8 @@ def client(chat):
     df_list = pd.DataFrame(
         {'Id': user_ids, 'Usernames': usernames, 'First_name': first_names,
          'Last_names': last_names, 'standart_phones': standart_phones})
-    # df_list.to_csv(f'{chat}.csv', sep=';', header=True, index=False)
     print(df_list)
     app.disconnect()
-    # app.run_until_disconnected()
     return df_list
 
 
@@ -93,12 +127,12 @@ def people_parse(update, context):
         chat = context.user_data['chat_name']
 
     set_event_loop(new_event_loop())
-    df_list = client(chat)
+    df_list = get_people(chat) #получаем данные
     df_list.to_csv(f'{chat}.csv', sep=';', header=True, index=False)
     path = os.path.abspath(f'{chat}.csv')
     context.bot.send_document(
         chat_id=user_chat.id,
-        document= open(f'{path}', 'rb')
+        document=open(f'{path}', 'rb')
     )
     os.remove(path)
 
