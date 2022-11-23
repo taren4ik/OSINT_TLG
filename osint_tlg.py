5a7011a3496278005e92b0fabdd3f38e191af8a0
Dmitry Permyakov <taren4ik@mail.ru>
2022-11-23 20:57:12 +1000

    Добавлено считывание сообщений в get_chanel

--- osint_tlg.py
+++ osint_tlg.py
@@ -1,13 +1,13 @@
 # TODO ограничение на крличество бесплатных запросов
-import os
-
-import pandas as pd
 import logging
-from telethon.sync import TelegramClient
+import os
+import pandas as pd
+import types
+from asyncio import set_event_loop, new_event_loop
 from dotenv import load_dotenv
 from telegram import ReplyKeyboardMarkup, Bot
 from telegram.ext import CommandHandler, MessageHandler, Updater, Filters
-from asyncio import set_event_loop, new_event_loop
+from telethon.sync import TelegramClient
 
 load_dotenv()
 
@@ -37,7 +37,7 @@ def wake_up(update, context):
 def parsing(update, context):
     """Выбор типа парсинга."""
     chat = update.effective_chat
-    buttons = ReplyKeyboardMarkup([['/people'], ['/message']],
+    buttons = ReplyKeyboardMarkup([['/people'], ['/message'], ['/chanel']],
                                   resize_keyboard=True)
     # Использую контекст context.user_data
     context.user_data['chat_name'] = update.message.text
@@ -63,7 +63,8 @@ def get_message(chat):
         msg_id.append(str(msg.sender_id))
         msg_dt.append(str(msg.date))
     df_list = pd.DataFrame(
-        {'Id': msg_id, 'DateTime': msg_dt, 'Message': msg_txt})
+        {'Id_user': msg_id, 'DateTime': msg_dt, 'Message': msg_txt})
+    df_list.sort_values(by='Id_user', ascending=False)
     app.disconnect()
     return df_list
 
@@ -141,6 +142,73 @@ def people_parse(update, context):
     os.remove(path)
 
 
+def get_chanel(channel):
+    """Парсинг сообщений."""
+    post_id = []
+    post_message = []
+    post_date = []
+    
+    url = f'https://t.me/{channel}'
+
+    with TelegramClient('osint', api_id, api_hash) as client:
+        messages_total = client.get_messages(channel).total
+        offset_msg = messages_total
+        while True:
+            # messages_total = client.get_messages(channel).total
+            # offset_msg = messages_total
+            post = client.get_messages(channel, ids=offset_msg - 1)  # пост
+
+            post_id.append(str())
+            post_message.append(post.message)
+            post_date.append(str(post.date.day) + '.' +
+                             str(post.date.month) + '.' +
+                             str(post.date.year))
+
+            result = client(functions.messages.GetRepliesRequest(
+                peer=channel,
+                msg_id=offset_msg,
+                offset_id=0,
+                offset_date=0,
+                add_offset=0,
+                limit=100,
+                max_id=0,
+                min_id=0,
+                hash=0
+            ))
+    messages = app.get_messages(url,  5000)
+    for msg in messages:
+        post.append(str(msg.message))
+        id.append(str(msg.sender_id))
+        dt.append(str(msg.date))
+        user_id.append(str(msg.date))
+    df_list = pd.DataFrame(
+        {'Id':  comment_id, 'DateTime': dt, 'Message': post,
+         'IdUser': user_id})
+    app.disconnect()
+    return df_list
+
+
+def chanel_parse(update, context):
+    """Парсинг сообщений."""
+    user_chat = update.effective_chat
+    context_user = context.user_data['chat_name']
+    if context_user.find(r'https://t.me/') != -1:
+        chat = context.user_data['chat_name'].split(r'/')[3]
+    else:
+        chat = context.user_data['chat_name']
+    get_users(user_chat, chat)  # логирование пользователей.
+    set_event_loop(new_event_loop())
+    df_list = get_chanel(chat)
+    df_list.to_csv(f'{chat}.csv', sep=';', header=True, index=False,
+                   encoding='utf-8')
+    path = os.path.abspath(f'{chat}.csv')
+    context.bot.send_document(
+        chat_id=user_chat.id,
+        document=open(f'{path}', 'rb')
+    )
+    os.remove(path)
+
+
 def get_users(user_chat, chat):
     """БД пользователей ."""
     df_users = pd.DataFrame(
@@ -157,6 +225,9 @@ def main():
 
     updater.dispatcher.add_handler(
         CommandHandler('people', people_parse))
+
+    updater.dispatcher.add_handler(
+        CommandHandler('chanel', chanel_parse))
     updater.dispatcher.add_handler(MessageHandler(Filters.text, parsing))
 
     updater.start_polling()
