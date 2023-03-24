e62a71398f3a19eaa010450f386478b1ebe53d2d
Dmitry Permyakov <taren4ik@mail.ru>
2023-03-24 16:20:55 +1000

    Новый .gitignore

--- osint_tlg.py
+++ osint_tlg.py
@@ -7,6 +7,8 @@
 
 import logging
 import os
+from datetime import time
+
 import pandas as pd
 from telethon import functions
 from asyncio import set_event_loop, new_event_loop
@@ -15,6 +17,7 @@ from telegram import ReplyKeyboardMarkup, Bot
 from telegram.ext import CommandHandler, MessageHandler, Updater, Filters
 from telethon.tl.types import ChannelParticipantsAdmins
 from telethon.sync import TelegramClient
+
 
 load_dotenv()
 
@@ -29,6 +32,8 @@ logging.basicConfig(
     level=logging.INFO)
 
 BUTTONS = ['chat_users', 'chat_messages', 'chanel_users']
+app = TelegramClient('osint', api_id, api_hash)
+app.start()
 
 
 def wake_up(update, context):
@@ -63,8 +68,8 @@ def get_message(chat):
     msg_txt = []
     url = f'https://t.me/{chat}'
 
-    app = TelegramClient('osint', api_id, api_hash)
-    app.start()
+    # app = TelegramClient('osint', api_id, api_hash)
+    # app.start()
     messages = app.get_messages(url, 5000)
     for msg in messages:
         msg_txt.append(str(msg.message))
@@ -117,50 +122,152 @@ def get_chat(chat):
     return df_list
 
 
-def get_chanel(channel):
+
+async def get_comment(channel, offset_msg, offset):
+    result = await app(functions.messages.GetRepliesRequest(
+        peer=channel,
+        msg_id=offset_msg,
+        offset_id=0,
+        offset_date=0,
+        add_offset=offset,
+        limit=100,
+        max_id=0,
+        min_id=0,
+        hash=0
+    ))
+    return result
+
+async def get_chanel(channel):
     """Парсинг сообщений."""
+    user_ids = []
+    first_names = []
+    last_names = []
+    usernames = []
+    phones = []
+    bots = []
+    ids_comment = []
+    messages_comment = []
+    dates_comment = []
     post_id = []
     post_message = []
     post_date = []
+    users_data = {}
+    # client = TelegramClient('osint', api_id, api_hash)
+    # client.start()
 
-    url = f'https://t.me/{channel}'
-
-    with TelegramClient('osint', api_id, api_hash) as client:
-        messages_total = client.get_messages(channel).total
+    client_msg = await app.get_messages(channel)
+    if client_msg[0].id:
+        messages_total = client_msg[0].id
         offset_msg = messages_total
-        while True:
-            # messages_total = client.get_messages(channel).total
-            # offset_msg = messages_total
-            post = client.get_messages(channel, ids=offset_msg - 1)  # пост
+    else:
+        print('нет поста!!!')
+    # offset_msg = 0
 
-            post_id.append(str())
-            post_message.append(post.message)
+    while offset_msg > 0:  # глубина сканирования сообщений в канале
+        client_msg = await app.get_messages(channel, ids=offset_msg)
+        if client_msg is not None:
+            post = client_msg
+
+            if post.message == '':
+                offset_msg = offset_msg - 1
+                continue
+            else:
+                post_message.append(post.message)
+
+            post_id.append(str(post.id))
             post_date.append(str(post.date.day) + '.' +
                              str(post.date.month) + '.' +
                              str(post.date.year))
 
-            result = client(functions.messages.GetRepliesRequest(
-                peer=channel,
-                msg_id=offset_msg,
-                offset_id=0,
-                offset_date=0,
-                add_offset=0,
-                limit=100,
-                max_id=0,
-                min_id=0,
-                hash=0
-            ))
-    messages = app.get_messages(url, 5000)
-    for msg in messages:
-        post.append(str(msg.message))
-        id.append(str(msg.sender_id))
-        dt.append(str(msg.date))
-        user_id.append(str(msg.date))
-    df_list = pd.DataFrame(
-        {'Id': comment_id, 'DateTime': dt, 'Message': post,
-         'IdUser': user_id})
-    app.disconnect()
-    return df_list
+        else:
+            offset_msg = offset_msg - 1
+
+        df_post = pd.DataFrame(
+            {'Id': post_id, 'Date': post_date,
+             'Message': post_message})
+
+        df_post.to_csv(f'{channel.split("/")[1]}.csv', mode='a',
+                       sep=':',
+                       header=True,
+                       index=False,
+                       encoding='utf-16')
+
+        try:
+            part_of_division = client_msg.replies.replies // 100
+
+            while part_of_division >= 0:
+                result = await get_comment(channel, offset_msg,
+                                           part_of_division * 100)
+
+                for user in result.users:
+                    user_ids.append(str(user.id))
+                    first_names.append(str(user.first_name))
+                    last_names.append(str(user.last_name))
+                    usernames.append('@' + str(user.username))
+                    phones.append(str(user.phone))
+                    bots.append(str(user.bot))
+
+                    if f'{user.id}' in users_data:
+                        count = users_data[f'{user.id}'] + 1
+                        users_data[f'{user.id}'] = count
+                    else:
+                        users_data[f'{user.id}'] = 1
+
+                for msg in result.messages:
+                    ids_comment.append(str(msg.sender_id))
+                    messages_comment.append(str(msg.message))
+                    dates_comment.append(str(msg.date.day) + '.' +
+                                         str(msg.date.month) + '.' +
+                                         str(msg.date.year))
+
+                df_users = pd.DataFrame(
+                    {'Id': user_ids,
+                     'Username': usernames,
+                     'First_name': first_names,
+                     'Last_names': last_names,
+                     'Phone': phones,
+                     'isBot': bots})
+
+                df_messages = pd.DataFrame(
+                    {'Id': ids_comment,
+                     'Date': dates_comment,
+                     'Message': messages_comment})
+
+                # sorted_df_messages = df_messages.sort_values(by='Id')
+
+                df_comments = df_messages.merge(df_users, on='Id',
+                                                how='left')
+
+                df_comments = df_comments.drop_duplicates(subset='Message')
+                part_of_division -= 1
+
+            if df_comments.size != 0:
+                df_comments.to_csv(f'{channel.split("/")[1]}.csv',
+                                   mode='a',
+                                   sep=';',
+                                   header=True,
+                                   index=False,
+                                   encoding='utf-16')
+
+        except:
+            print(f'Нет информации по посту!!!!ID: {str(post.id)}')
+
+        offset_msg = offset_msg - 1
+        ids_comment = []
+        messages_comment = []
+        dates_comment = []
+
+        post_id = []
+        post_message = []
+        post_date = []
+        df_messages = df_messages[0:0]
+        df_users = df_users[0:0]
+        df_comments = df_comments[0:0]
+        time.sleep(0.5)
+    sorted_tuple = sorted(users_data.items(), key=lambda x: x[1], reverse=True)
+    users_data = dict(sorted_tuple)
+
+    return users_data
 
 
 def get_report(update, context):
